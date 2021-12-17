fec04033a13df36f9a0ee61980c88d35cfc02fc6
Mahmoud elsawah <hxxcvhh122@gmail.com>
2021-12-17 21:02:52 +0200

    first-comment

---
+++ js/jss/evenfly.js
@@ -0,0 +1,14 @@
+//========================
+//CUSTOM SCROLLBAR
+//========================
+$(document).ready(
+    function () {
+        $("html").niceScroll({
+            mousescrollstep: 70,
+            cursorcolor: "#f53f2c",
+            cursorwidth: "5px",
+            cursorborderradius: "10px",
+            cursorborder: "none",
+        });
+    }
+);
